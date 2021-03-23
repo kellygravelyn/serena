bc6aec89c623a29f145cd73eda32ad94a07ee903
Nick Gravelyn <nick@nickgravelyn.com>
2021-03-22 19:48:04 -0700

    Most basic static site server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    warp::serve(warp::fs::dir("./example-site/"))
+        .run(([127, 0, 0, 1], 3000))
+        .await;
 }

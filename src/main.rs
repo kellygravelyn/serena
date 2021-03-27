abc0f5208827ae6eb121f8f6bc98b40512f36f6a
Nick Gravelyn <nick@nickgravelyn.com>
2021-03-26 21:31:46 -0700

    Moving static file handling to a separate module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::{net::SocketAddr, path::{Path, PathBuf}, time::Duration};
+use std::{net::SocketAddr, time::Duration};
 
-use tokio::{fs::File, io::AsyncReadExt, sync::mpsc::UnboundedSender, time::sleep}; 
+use tokio::{sync::mpsc::UnboundedSender, time::sleep}; 
 use tokio_stream::wrappers::UnboundedReceiverStream;
-use tokio_util::codec::{BytesCodec, FramedRead};
 use hyper::{Body, Error, Request, Response, Result, Server, service::{make_service_fn, service_fn}};
 
 mod opts;
 mod watch;
+mod statics;
 
 #[tokio::main]
 async fn main() {
@@ -44,7 +44,7 @@ async fn routes(req: Request<Body>, root_dir: String, refresh_receiver: tokio::s
     if path == "/__tennis" {
         refresh_events(refresh_receiver).await
     } else {
-        transfer_static_file(path, root_dir).await
+        statics::transfer_static_file(path, root_dir).await
     }
 }
 
@@ -91,64 +91,4 @@ fn map_refresh_events(sender: UnboundedSender<Result<String>>, mut refresh_recei
             }
         }  
     });
-}
-
-fn not_found() -> Result<Response<Body>> {
-    Ok(
-        Response::builder()
-            .status(404)
-            .body(Body::from(""))
-            .unwrap()
-    )
-}
-
-async fn transfer_static_file(path: &str, root_dir: String) -> Result<Response<Body>> {
-    let filepath = build_file_path(&path, &root_dir);
-    if let Ok(file) = File::open(&filepath).await {
-        if is_html_file(&filepath) {
-            html_response(file).await
-        } else {
-            file_stream_response(file).await
-        }
-    } else {
-        not_found()
-    }
-}
-
-async fn html_response(mut file: File) -> Result<Response<Body>> {
-    let mut html = String::new();
-    if let Ok(_) = file.read_to_string(&mut html).await {
-        watch::attach_script(&mut html);
-        Ok(Response::new(Body::from(html)))
-    } else {
-        Ok(
-            Response::builder()
-                .status(500)
-                .body(Body::from("Failed to read file"))
-                .unwrap()
-        )
-    }
-}
-
-async fn file_stream_response(file: File) -> Result<Response<Body>> {
-    let stream = FramedRead::new(file, BytesCodec::new());
-    let body = Body::wrap_stream(stream);
-    Ok(Response::new(body))
-}
-
-fn is_html_file(filepath: &Path) -> bool {
-    if let Some(ext) = filepath.extension() {
-        ext == "html"
-    } else {
-        false
-    }
-}
-
-fn build_file_path(path: &str, root_dir: &String) -> PathBuf {
-    let trimmed_characters: &[_] = &['/', '.'];
-    let mut filepath = Path::new(&root_dir).join(path.trim_start_matches(trimmed_characters));
-    if filepath.is_dir() {
-        filepath = filepath.join("index.html");
-    }
-    filepath
 }
